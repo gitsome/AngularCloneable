63db391573f90ac3c138141d5fc791eeb2a648ff
John David Martin <johndavidfive@gmail.com>
2016-07-12 20:05:34 -0700

    Syntax and tweaks

--- classes/Cloneable.cls.js
+++ classes/Cloneable.cls.js
@@ -2,15 +2,13 @@
 
     /*============= EXTEND ANGULAR (1.5.5) ============*/
 
-    angular.isClonable = function (item) {
+    angular.isCloneable = function (item) {
         return item._AngularCloneable === true;
     };
-
 
     /**
      * A base class that adds a clone method that recursively makes a deep copy and re-instantiates objects that have also extended this class
      */
-
     angular.module('AngularCloneable').factory('Cloneable', [
 
         function () {
@@ -24,28 +22,33 @@
 
                 var that = this;
 
+
                 /*============ PRIVATE VARIABLES AND METHODS ============*/
 
                 // we have introduced a clonable contract, that invokes the clone method at the root level
                 // if however, we find nested objects that are also clonable, we should clone them too
                 // this will create a recursive search for clonable items
-                var reinstantiateClonableItems = function (item) {
+                var reinstantiateCloneableItems = function (item) {
 
-                    if (angular.isClonable(item)) {
+                    if (angular.isCloneable(item)) {
 
                         return item.clone();
 
                     } else if (angular.isArray(item)) {
 
                         angular.forEach(item, function (arrayItem, arrayIndex) {
-                            item[arrayIndex] = reinstantiateClonableItems(arrayItem);
+                            item[arrayIndex] = reinstantiateCloneableItems(arrayItem);
                         });
+
+                        return item;
 
                     } else if (angular.isObject(item)) {
 
                         angular.forEach(item, function (itemValue, itemKey) {
-                            item[itemKey] = reinstantiateClonableItems(itemValue);
+                            item[itemKey] = reinstantiateCloneableItems(itemValue);
                         });
+
+                        return item;
 
                     } else {
 
@@ -69,7 +72,7 @@
                     // That is in most cases ultimately okay because you will end up with an object in the same state as if the constructor was called
                     // The downside is that if during the constructor an event is fired or external service's state is changed you would lose that during cloning
                     // If we wanted to add support for nested cloning that would run all class constructors we would need to alter angular.copy or write our own custom copy method
-                    var clonedItem = new instance.constructor(angular.copy(instance));
+                    var clonedItem = new instance.constructor(angular.copy(instance)); // what's so great is angular.copy is already a great and well tested deep copy that covers byteArrays and all kinds of goodness
 
                     return reinstantiateCloneableItems(clonedItem);
                 };
